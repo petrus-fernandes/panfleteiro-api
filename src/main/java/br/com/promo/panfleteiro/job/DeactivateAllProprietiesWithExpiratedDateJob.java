fb26baa9815eb65a9ba31e583c8dcc8bd5b1d408
petrus-f <petrus_fernandes@hotmail.com>
2025-06-10 20:32:08 -0300

    ativação do job para desativar anúncios vencidos

--- src/main/java/br/com/promo/panfleteiro/job/DeactivateAllProprietiesWithExpiratedDateJob.java
+++ src/main/java/br/com/promo/panfleteiro/job/DeactivateAllProprietiesWithExpiratedDateJob.java
@@ -1,5 +1,6 @@
 package br.com.promo.panfleteiro.job;
 
+import br.com.promo.panfleteiro.helper.AdMarketHelper;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
@@ -9,14 +10,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 public class DeactivateAllProprietiesWithExpiratedDateJob implements Job {
 
-//    @Autowired
-//    FlyerOrchestrator flyerOrchestrator;
+    @Autowired
+    private AdMarketHelper adMarketHelper;
 
     private static final Logger logger = LoggerFactory.getLogger(DeactivateAllProprietiesWithExpiratedDateJob.class);
     @Override
     public void execute(JobExecutionContext context) throws JobExecutionException {
         logger.info("Initiate deactivation of Ads, FlyerSections and Flyers expirated.");
-//        flyerOrchestrator.deactivateEntitiesByExpiratedDate();
+        adMarketHelper.deactivateEntitiesByExpiratedDate();
         logger.info("Deactivation of Ads, FlyerSections and Flyers expirated completed.");
     }
 }
